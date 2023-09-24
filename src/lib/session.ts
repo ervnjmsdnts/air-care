1adc36e953eaa0b19e47bb72368f5a357486f1d3
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-09-25 00:43:05 +0800

    issue

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 export async function getUser() {
   const cookie = cookies().get('session')?.value;
   const res = await fetch(
-    `${process.env.VERCEL_URL ?? 'http://localhost:3000'}/api/user`,
+    `https://${process.env.VERCEL_URL ?? 'http://localhost:3000'}/api/user`,
     {
       method: 'GET',
       headers: { cookie: `jwt=${cookie}` },
