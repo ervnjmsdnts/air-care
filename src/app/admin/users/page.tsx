6599f80b4c653af0456c55eb54f71257c4b576f2
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-10-05 19:11:22 +0800

    fix

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,6 +1,8 @@
 import UserTable from './(components)/user-table';
 import { serverClient } from '@/app/_trpc/server';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Users() {
   const users = await serverClient.getUsers();
   const user = await serverClient.getCurrentUser();
