89903eb5f4a935e2495928664d28d0d8bdd52985
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-10-05 19:24:44 +0800

    fix

--- src/app/admin/records/audits/page.tsx
+++ src/app/admin/records/audits/page.tsx
@@ -1,8 +1,6 @@
 import { serverClient } from '@/app/_trpc/server';
 import AuditTable from './(components)/audit-table';
 
-export const dynamic = 'force-dynamic';
-
 export default async function Audits() {
   const audits = await serverClient.getAudits();
 
