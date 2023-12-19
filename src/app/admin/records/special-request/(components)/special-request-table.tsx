5d142437200c9df0abafc60ab5a11c3a1143360c
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-12-19 23:45:36 +0800

    update

--- src/app/admin/records/special-request/(components)/special-request-table.tsx
+++ src/app/admin/records/special-request/(components)/special-request-table.tsx
@@ -1,14 +1,41 @@
+'use client';
+
 import { DataTable } from '@/components/ui/data-table';
 import { SpecialRequest, User } from '@prisma/client';
 import { specialRequestColumn } from '../columns';
+import { useMemo } from 'react';
+import { Button } from '@/components/ui/button';
+import { CSVLink } from 'react-csv';
 
 export default function SpecialRequestTable({
   requests,
 }: {
   requests: (SpecialRequest & { user: User })[];
 }) {
+  const csvData = useMemo(
+    () =>
+      requests.map((request) => ({
+        User: request.user.name,
+        Image: request.url ? request.url : '',
+        Brand: request.brand,
+        Model: request.model,
+        Description: request.description ? request.description : '',
+        Status: request.status,
+      })),
+    [requests],
+  );
+
+  console.log(csvData);
+
   return (
     <div className='flex flex-col h-full gap-2'>
+      <div className='flex justify-end'>
+        <Button asChild>
+          <CSVLink data={csvData} filename='special-request.csv'>
+            Export CSV
+          </CSVLink>
+        </Button>
+      </div>
       <DataTable
         data={requests}
         columns={specialRequestColumn}
